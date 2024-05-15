912e3942ea4704069a9e3f3e1bb3fd4ae680a5fa
Bolaji <bolajialabi104@gmail.com>
2024-05-15 06:13:30 +0100

    first commit

---
+++ js intro/app.js
@@ -0,0 +1,28 @@
+var firstname = "Bolaji";
+var Lastname = "Bolaji";
+console.log(firstname + " "+ Lastname);
+console.log("My fullname is " + +firstname +" " + Lastname);
+
+// int data
+var num1 = 50;
+var num2 = 50;
+console.log(num1 * num2);
+
+// float data
+var num3 = 60.7;
+var num4 = 0.05;
+console.log(num3 * num4);
+
+// array data
+var favGame = ["Football", "Basketball","Volleyball"];
+console.log(favGame)
+
+// object data
+var myDetails = {
+    fullname: "John Doe" ,
+    age: 25,
+    isMarried: true,
+    favGame: ["Football", "Basketball", "Volleyball"]
+}
+console.log(myDetails);
+
---
+++ tailwind_essential/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./buld/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
